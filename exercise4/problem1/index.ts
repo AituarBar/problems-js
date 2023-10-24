d0a7030d6813f9b6dd56a54e92d2162ad21972fc
Aituar <47512436+AituarBar@users.noreply.github.com>
2023-10-24 15:03:10 +0600

    Update index.ts

--- exercise4/problem1/index.ts
+++ exercise4/problem1/index.ts
@@ -1,3 +1,24 @@
-class Person {}
+class Person {  
+    name: string;
+    age: number;
+ 
+    constructor(name: string, age:number) {
+         this.name = name;
+         this.age = age;
+     }
+       compareAge(other: Person):string{
+       if(this.age>other.age){
+          return `${other.name} is younger than me.`;
+       }
+        if(this.age<other.age){
+          return `${other.name} is older than me.`;
+       }
+      else{
+          return `${other.name} is the same age as me.`;
+       }
+    }}
 
+const ildar = new Person("Ildar", 22)
+const ivan = new Person("Ivan", 23)
+console.log(ildar.compareAge(ivan))
 export default Person;
