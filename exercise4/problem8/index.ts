e14f2e8e7c5f7d82947a32b126ad502d86f5ea92
Aituar <47512436+AituarBar@users.noreply.github.com>
2023-10-24 15:08:27 +0600

    Update index.ts

--- exercise4/problem8/index.ts
+++ exercise4/problem8/index.ts
@@ -1,3 +1,34 @@
-class Calculator {}
+class Calculator {
+    value: number;
+    constructor(value: number) {
+        this.value = value;
+    }
+    add(num: number){
+       return this.value += num;
+    }
+
+    subtract(num: number) {
+       return this.value -= num;
+       
+    }
+
+    divide(num: number) {
+        if (num == 0) {
+           return "Can not divide by 0!";
+        }
+       return this.value /= num;
+      
+    }
+
+    multiply(num: number){
+        return this.value *= num;
+         
+    }
+
+    logResult():number{
+        
+        return this.value;
+    }
+}
 
 export default Calculator;
